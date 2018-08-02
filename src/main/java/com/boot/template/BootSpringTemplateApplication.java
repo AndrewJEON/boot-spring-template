de70545aacc1ae6ba24b2a08037dd3b7d654ec95
SungHyun Cheong <hashmap27@gmail.com>
2018-08-02 09:41:34 +0900

    first commit

---
+++ src/main/java/com/boot/template/BootSpringTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.boot.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSpringTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSpringTemplateApplication.class, args);
+	}
+}
