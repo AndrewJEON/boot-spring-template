02195bd2e2957a47ff2916f65ad6a9e9e8fa0851
SungHyun Cheong <hashmap27@gmail.com>
2018-08-03 18:11:10 +0900

    controller, service, repository support 생성.

---
+++ src/main/java/com/boot/template/framework/support/base/TemplateServiceSupport.java
@@ -0,0 +1,10 @@
+package com.boot.template.framework.support.base;
+
+/**
+ * Javabase Spring Template Service Support
+ *
+ * @author Cheong SungHyun <hashmap27@gmail.com>
+ */
+public abstract class TemplateServiceSupport extends TemplateComponentSupport {
+
+}
